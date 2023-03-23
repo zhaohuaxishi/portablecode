73fb192806853d632cd0ec285ca3f97cc2aa32ad
guorongfei <guorongfei@cvte.com>
2023-03-23 11:04:34 +0800

    refactor: add testcase

---
+++ random_uuid_boost/test_random_uuid.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <regex>
+#include <set>
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+#include "random_uuid.h"
+
+
+TEST_CASE("test random uuid") {
+    std::regex uuid_regex("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
+
+    SECTION("check random") {
+        std::set<std::string> generated;
+        for (int i = 0; i < 1000; ++i) {
+            const auto& uuid = CreateRandomUUID();
+            REQUIRE(std::regex_search(uuid, uuid_regex));
+            REQUIRE(generated.count(uuid) == 0);
+            generated.insert(uuid);
+        }
+    }
+}
